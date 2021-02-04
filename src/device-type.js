15bad364c5262f411af83c0134072432d283a69b
Ivan Brezhnev <brezhnev.ivan@yahoo.com>
2021-02-04 15:41:07 +0200

    SKY2-0. Detect mobile user agent based on window width

--- src/device-type.js
+++ src/device-type.js
@@ -1,6 +1,2 @@
-import Bowser from 'bowser';
-
-const browser = Bowser.parse(window.navigator.userAgent);
-
-export const isMobileBrowser = () => browser.platform.type === 'mobile' || window.innerWidth <= 512;
-export const isDesktopBrowser = () => browser.platform.type === 'desktop' || window.innerWidth > 512;
+export const isMobileBrowser = () => window.innerWidth <= 512;
+export const isDesktopBrowser = () => window.innerWidth > 512;
