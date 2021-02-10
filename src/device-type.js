a26330a4ed36d0b94a10f38d63506b3042cb65be
Ivan Brezhnev <brezhnev.ia@gbksoft.com>
2021-02-10 12:31:17 +0200

    SKY2-0. Changes the limit of the screen width at which detects mobile view

--- src/device-type.js
+++ src/device-type.js
@@ -1,2 +1,2 @@
-export const isMobileBrowser = () => window.innerWidth <= 512;
-export const isDesktopBrowser = () => window.innerWidth > 512;
+export const isMobileBrowser = () => window.innerWidth < 1280;
+export const isDesktopBrowser = () => window.innerWidth >= 1280;
