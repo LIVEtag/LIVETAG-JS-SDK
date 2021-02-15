a09085b2df76f01447ea380afafa8465d16bc012
Ivan Brezhnev <brezhnev.ivan@yahoo.com>
2021-02-15 11:46:53 +0200

    SKY2-0. Install theme "yuu" for docs

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -36,11 +36,13 @@ module.exports = {
    * ref：https://vuepress.vuejs.org/config/#head
    */
   head: [
+    ['meta', { charset: 'utf-8' }],
+    ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0' }],
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
   ],
-
+  theme: 'yuu',
   /**
    * Theme configuration, here is the default theme configuration for VuePress.
    *
