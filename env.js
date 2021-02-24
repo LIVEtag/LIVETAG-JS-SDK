8e509a74b9597be3639ddcd74b9abde105ddf4f7
Ivan Brezhnev <brezhnev.ivan@yahoo.com>
2021-02-24 19:03:41 +0200

    SKY2-0. Fix typo

--- env.js
+++ env.js
@@ -4,6 +4,6 @@ dotenv.config({ path: '.env' });
 
 module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
-  APP_WIDGET_URL: process.env.APP_SDAPP_WIDGET_URLK_URL,
+  APP_WIDGET_URL: process.env.APP_WIDGET_URL,
   APP_SDK_URL: process.env.APP_SDK_URL,
 };
